826d353a40203745e0098b5aaac5761f5bc0b98f
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-24 13:54:33 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LibraryAgent from './LibraryAgent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LibraryAgent />
+  </React.StrictMode>
+)
